57e070fc69b5fb11412c4e88734111896b3dd889
yjh8806 <yjh8806@hotmail.com>
2021-06-08 22:42:55 +0900

    20210608 api updates

--- app.py
+++ app.py
@@ -1,37 +1,32 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 import requests
 from bs4 import BeautifulSoup
-# 순서 섞기 위해
-import random
-
-#############################
-# (입맛에 맞게 코딩)
-#############################
-app = Flask(__name__)
-
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
-db = client.NikeShoe
+# to encrypt PW
+import jwt
+# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용
+import datetime
+# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장
+# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있음
+import hashlib
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+# 순서 섞기
+import random
 
-# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
-# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
+# JWT 토큰을 만들 때 필요한 비밀문자열. 아무거나 입력O
+# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 가능
 SECRET_KEY = 'SPARTA'
 
-# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
-import jwt
+app = Flask(__name__)
 
-# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
-import datetime
-
-# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
-# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
-import hashlib
 client = MongoClient('localhost', 27017)
-db = client.NikeShoe
+db = client.Shoe
 
 def shoes_data():
-    db.shoes.drop()
+    # db.shoes.drop()
+    db.shoes.remove({})
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get('https://www.shoeprize.com/today/', headers=headers)
@@ -39,16 +34,42 @@ def shoes_data():
 
     trs = soup.select('body > div.container > div.content > div.product_list_area.current_list > ul > li')
     for tr in trs:
+        image = tr.select_one('div.img_area > a > img')['data-src']
         shop = tr.select_one('div.info_area > div.text_area > div.brand > a').text
         shoe = tr.select_one('div.info_area > div.text_area > div.name').text
         country = tr.select_one('div.info_area > div.text_area > div.delivery').text
+        link = tr.select_one('div.img_area > a')['href']
         doc = {
+            'image': image,
             'shop': shop,
             'shoe': shoe,
-            'country': country
+            'country': country,
+            'link': link,
         }
         db.shoes.insert_one(doc)
 
+def oldShoes_data():
+    db.old_shoes.remove({})
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://www.shoeprize.com/today/', headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    old_shoes = soup.select('body > div.container > div.content > div:nth-child(5) > ul > li')
+    for old_shoe in old_shoes:
+        image = old_shoe.select_one('div.img_area > a > img')['data-src']
+        shop = old_shoe.select_one('div.info_area > div.text_area > div.brand > a').text
+        shoe = old_shoe.select_one('div.name').text
+        country = old_shoe.select_one('div.delivery').text
+        link = old_shoe.select_one('div.img_area > a')['href']
+
+        doc = {
+            'image': image,
+            'shop': shop,
+            'shoe': shoe,
+            'country': country,
+            'link': link,
+        }
+        db.old_shoes.insert_one(doc)
 #################################
 ##  HTML을 주는 부분             ##
 #################################
@@ -60,108 +81,69 @@ def home():
         user_info = db.user.find_one({"id": payload['id']})
         return render_template('index.html', nickname=user_info["nick"])
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        return redirect(url_for("login", msg="time has been expired"))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
+        return redirect(url_for("login", msg="Login Failed"))
 
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-
-#################################
-##  로그인을 위한 API            ##
-#################################
-
-# [회원가입 API]
-# id, pw, nickname을 받아서, mongoDB에 저장합니다.
-# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
-@app.route('/api/register', methods=['POST'])
-def api_register():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-    nickname_receive = request.form['nickname_give']
-
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
-
-    return jsonify({'result': 'success'})
-
-
-# [로그인 API]
-# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
-@app.route('/api/login', methods=['POST'])
-def api_login():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-
-    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
-
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
     if result is not None:
-        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
-        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
-        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
-        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
         payload = {
-            'id': id_receive,
-            # exp 5초 후 로그인 만
-            # 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60)  # login 1hr
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-
-        # token을 줍니다.
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+        return jsonify({'result': 'fail', 'msg': 'ID or password does not match.'})
 
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,
+        "password": password_hash,
+        "profile_name": username_receive,
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 
-# [유저 정보 확인 API]
-# 로그인된 유저만 call 할 수 있는 API입니다.
-# 유효한 토큰을 줘야 올바른 결과를 얻어갈 수 있습니다.
-# (그렇지 않으면 남의 장바구니라든가, 정보를 누구나 볼 수 있겠죠?)
-@app.route('/api/nick', methods=['GET'])
-def api_valid():
-    token_receive = request.cookies.get('mytoken')
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    # print(value_receive, type_receive, exists)
+    return jsonify({'result': 'success', 'exists': exists})
 
-    # try / catch 문?
-    # try 아래를 실행했다가, 에러가 있으면 except 구분으로 가란 얘기입니다.
-
-    try:
-        # token을 시크릿키로 디코딩합니다.
-        # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        print(payload)
-
-        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
-        # 여기에선 그 예로 닉네임을 보내주겠습니다.
-        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
-        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
-    except jwt.ExpiredSignatureError:
-        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
-        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
-    except jwt.exceptions.DecodeError:
-        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
-
-@app.route('/api/list', methods=['GET'])
+@app.route('/api/mypage', methods=['GET'])
+def tomypage():
+    msg = request.args.get("msg")
+    return render_template('mypage.html', msg=msg)
+# 메인 페이지
+@app.route('/api/list/new', methods=['GET'])
 def show_shoes():
     shoes_data()
     lists = list(db.shoes.find({}, {'_id': False}))
     return jsonify({'result': 'success', 'all_lists': lists})
 
+@app.route('/api/list/ended', methods=['GET'])
+def show_oldShoes():
+    oldShoes_data()
+    lists_2 = list(db.old_shoes.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'all_lists2': lists_2})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
